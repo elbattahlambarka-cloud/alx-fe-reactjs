ff5f64ee43384154f41057cee8e0d8f33eb97504
Lambarka <elbattahlambarka@gmail.com>
2025-11-21 14:21:26 +0100

    Fix Tailwind CSS configuration and PostCSS setup

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,8 @@
 import Search from './components/Search';
-import './App.css';
 
 function App() {
   return (
-    <div style={{ 
-      minHeight: '100vh',
-      backgroundColor: '#f6f8fa',
-      padding: '20px'
-    }}>
+    <div>
       <Search />
     </div>
   );
